0122c5148615a5bcf84d341fc739de89af54039e
TomorrowMarkets <cphtolon@gmail.com>
2026-09-23 15:08:37 +0100

    Implement EventBus class for event handling

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,1 +1,18 @@
+class EventBus {
+  constructor() {
+    this.events = {};
+  }
 
+  on(event, callback) {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(callback);
+  }
+
+  emit(event, data) {
+    if (this.events[event]) {
+      this.events[event].forEach(callback => callback(data));
+    }
+  }
+}
+
+export const events = new EventBus();
