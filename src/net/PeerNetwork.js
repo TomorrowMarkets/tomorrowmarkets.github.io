7e257e0e310916633efc5f1116c185085c0eb9b3
TomorrowMarkets <cphtolon@gmail.com>
2026-09-23 15:14:27 +0100

    Implement PeerNetwork class for peer communication

--- src/net/PeerNetwork.js
+++ src/net/PeerNetwork.js
@@ -1,1 +1,23 @@
 
+import { events } from '../EventBus.js';
+
+export class PeerNetwork {
+  constructor() {
+    this.peer = null;
+    this.connections = [];
+
+    // Broadcast local trades to connected peers
+    events.on('TRADE_EXECUTED', (trade) => this.broadcast({ type: 'TRADE', trade }));
+  }
+
+  broadcast(message) {
+    this.connections.forEach(conn => conn.send(message));
+  }
+
+  onReceiveMessage(data) {
+    if (data.type === 'SUBMIT_ORDER') {
+      // Forward incoming peer order to local EventBus
+      events.emit('REMOTE_ORDER_RECEIVED', data.order);
+    }
+  }
+}
