ca8e94441e4f2b6a92ed03627734f16cabc6abb6
TomorrowMarkets <cphtolon@gmail.com>
2026-09-23 15:14:09 +0100

    Implement BookUI class for order book updates

--- src/ui/BookUI.js
+++ src/ui/BookUI.js
@@ -1,1 +1,22 @@
 
+import { events } from '../EventBus.js';
+
+export class BookUI {
+  constructor() {
+    this.bidsTable = document.getElementById('bids-body');
+    this.asksTable = document.getElementById('asks-body');
+
+    // Subscribe to OrderBook updates
+    events.on('ORDER_BOOK_UPDATED', (book) => this.render(book));
+  }
+
+  render({ bids, asks }) {
+    this.bidsTable.innerHTML = bids.map(b => 
+      `<tr class="bid"><td>$${b.price}</td><td>${b.size}</td></tr>`
+    ).join('');
+
+    this.asksTable.innerHTML = asks.map(a => 
+      `<tr class="ask"><td>$${a.price}</td><td>${a.size}</td></tr>`
+    ).join('');
+  }
+}
