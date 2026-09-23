4fcb0557c829a5a90597e256fd1e18b3423eb2ed
TomorrowMarkets <cphtolon@gmail.com>
2026-09-23 15:48:35 +0100

    Implement tick method in GameLoop class

--- src/game/GameLoop.js
+++ src/game/GameLoop.js
@@ -1,1 +1,12 @@
 
+// src/game/GameLoop.js
+tick() {
+  const marketState = {
+    clockSeconds: this.clockSeconds,
+    durationMinutes: this.durationMinutes,
+    midPrice: this.orderBook.getMidPrice()
+  };
+
+  // Run all active bot behaviors
+  this.bots.forEach(bot => bot.onTick(marketState));
+}
